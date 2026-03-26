6185dbbac6ba6523d7460ba9108113e6a1d01590
samanehnoori <samaneh.noori@motopp.nl>
2026-03-26 13:38:12 +0100

    Delete database.py

--- database.py
+++
@@ -1,6 +0,0 @@
-users_email = [
-    {"email": "zeki.tazegul@motopp.nl", "password": "123456"},
-    {"email": "samaneh.noori@motopp.nl", "password": "654321"}
-]
-
-current_user = {"email": None, "password": None}
