4dc58eeadb4d30e9c66228c8864e6b25df12c176
samanehshahhoseini <samaneh.noori@motopp.nl>
2026-03-26 13:23:48 +0100

    I have imported users_email database from register.py

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+# We import database models.
+from db import models
+from db.database import engine
 
-import register
-import login
-import signout
+# We import all routers
+from routers.user import router as user_router
+from routers.booking import router as booking_router
+from routers.preferences import router as preferences_router
+
+# Lifespan:  Create tables when app starts up.
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    models.Base.metadata.create_all(bind=engine)
+    yield
+
+# FastAPI app
+app = FastAPI(
+    title="Hotel Booking API",
+    version="1.0.0",
+    lifespan=lifespan
+)
+
+# We include routers.
+app.include_router(user_router)
+app.include_router(preferences_router)
+app.include_router(booking_router)
+
+
+# Basic test end point to see whether the app is alive :)
+@app.get("/")
+def root():
+    title = "Hotel Booking API is running , this is just for testing purpose."
+    return {"message": "Welcome to Hotel booking API. It is running."}
